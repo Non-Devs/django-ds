3d6e49697d49c6a5118c2991c48827056869ac83
Elias Bernardo <ebmm01@gmail.com>
2018-05-06 18:45:36 -0300

    Adicionando app User

---
+++ ped/user/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from django.contrib.auth.views import login as authLogin
+
+app_name = 'user'
+
+urlpatterns = [
+    path('entrar', authLogin,
+         {'template_name': 'user/login.html'},
+         name='login',
+         ),
+]
