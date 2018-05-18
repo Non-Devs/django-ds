1d9c0d1e64593166b911b6675aeacca729d52cb0
Elias Bernardo <ebmm01@gmail.com>
2018-05-18 14:12:35 -0300

    Adicionando a possibilidade de um usuário se registrar

--- ped/user/urls.py
+++ ped/user/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 from . import views
 from django.contrib.auth.views import login as authLogin
-
 app_name = 'user'
 
 urlpatterns = [
-    path('entrar', authLogin,
+    path('entrar',
+         authLogin,
          {'template_name': 'user/login.html'},
          name='login',
+         ),
+
+    path('registrar',
+         views.registrar,
+         name='register',
          ),
 ]
