f0e3f235cf2fc28da4c3a7cbdb33813f61f521b8
Elias Bernardo <ebmm01@gmail.com>
2018-06-26 09:42:43 -0300

    Ajustes finais no fórum

--- ped/forum/admin.py
+++ ped/forum/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+
 from .models import Thread, Reply
 
 
@@ -6,12 +7,13 @@ class ThreadAdmin(admin.ModelAdmin):
 
     list_display = ['title', 'author', 'created', 'modified']
     search_fields = ['title', 'author__email', 'body']
+    prepopulated_fields = {'slug': ('title',)}
 
 
 class ReplyAdmin(admin.ModelAdmin):
 
-    list_display = ['thread', 'author', 'created', 'modified']
-    search_fields = ['thread__title', 'reply', 'author__email']
+    list_display = ['thread', 'author', 'correct', 'created', 'modified']
+    search_fields = ['thread__title', 'author__email', 'reply']
 
 
 admin.site.register(Thread, ThreadAdmin)
--- ped/forum/urls.py
+++ ped/forum/urls.py
@@ -1,3 +1,4 @@
+
 from django.urls import path
 from . import views
 
@@ -5,4 +6,10 @@ app_name = 'forum'
 
 urlpatterns = [
     path('', views.ForumView.as_view(), name='index'),
+    path('tag/<tag>', views.ForumView.as_view(), name='index_tagged'),
+    path('respostas/<pk>/correta', views.ReplyCorrectView.as_view(), name='reply_correct'),
+    path('respostas/<pk>/incorreta', views.ReplyCorrectView.as_view(correct=False), name='reply_incorrect'),
+    path('<slug:slug>', views.ThreadView.as_view(), name='thread'),
+
+
 ]
